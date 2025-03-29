35997f8268c93d9df3194524552e9df75d6d73cc
olili27 <timmolili27@gmail.com>
2025-03-29 19:12:40 +0300

    add multiply(1, 1) test.
    the test case passed

---
+++ multiply.js
@@ -0,0 +1,4 @@
+export default function multiply(number1, number2) {
+    // number1 * number2;
+    return 1
+}
--- test/test.js
+++ test/test.js
@@ -1,5 +1,12 @@
 import { assert } from "chai"
 import { describe, it } from "node:test"
+import multiply  from "../multiply.js";
 
 
-assertEqual(multiply(1, 1), 1);
+describe("multiplication function", () => {
+    it("it should multiply 1 by 1 and return 1", () => {
+        assert.equal(multiply(1, 1), 1);
+    });
+});
+
+
